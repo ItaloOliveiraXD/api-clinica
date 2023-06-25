25075820d9b8cb7fea2feaf3bc78f901bcbd9593
Italo Oliveira <italooliveira-xd@hotmail.com>
2023-06-25 19:19:47 -0300

    Iniciando o projeto da api-clinica.

---
+++ src/main/java/br/com/projeto/apiclinica/ApiClinicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.apiclinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClinicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/apiclinica/ApiClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.apiclinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
