c0872bc39d9575f6469a3f7fd4a9e4c3ef70bcfb
ítalo.oliveira <italooliveira-xd@hotmail.com>
2023-06-26 08:55:40 -0300

    Adicionando o método para visualizar um médico.

--- src/main/java/br/com/projeto/apiclinica/api/controller/MedicoController.java
+++ src/main/java/br/com/projeto/apiclinica/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,12 +25,13 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
-    
+
     @Autowired
     private MedicoService medicoService;
 
     @PostMapping
-    public ResponseEntity<DetalheMedicoDto> cadastra(@RequestBody @Valid MedicoDto medicoDto, UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<DetalheMedicoDto> cadastra(@RequestBody @Valid MedicoDto medicoDto,
+            UriComponentsBuilder uriBuilder) {
 
         Medico medico = medicoService.cadastraMedico(medicoDto);
         URI uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
@@ -39,9 +41,16 @@ public class MedicoController {
 
     @GetMapping
     public ResponseEntity<Page<ListaMedicoDto>> lista(@PageableDefault(size = 5, sort = "nome") Pageable page) {
-        
+
         Page<ListaMedicoDto> medicos = medicoService.listaMedicos(page).map(ListaMedicoDto::new);
-        
+
         return ResponseEntity.ok().body(medicos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DetalheMedicoDto> visualiza(@PathVariable Long id) {
+
+        Medico medico = medicoService.pegaMedico(id);
+        return ResponseEntity.ok().body(new DetalheMedicoDto(medico));
     }
 }
--- src/main/java/br/com/projeto/apiclinica/domain/service/MedicoService.java
+++ src/main/java/br/com/projeto/apiclinica/domain/service/MedicoService.java
@@ -1,14 +1,13 @@
 package br.com.projeto.apiclinica.domain.service;
 
+import br.com.projeto.apiclinica.api.dto.medico.MedicoDto;
+import br.com.projeto.apiclinica.domain.models.Medico;
+import br.com.projeto.apiclinica.domain.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import br.com.projeto.apiclinica.api.dto.medico.MedicoDto;
-import br.com.projeto.apiclinica.domain.models.Medico;
-import br.com.projeto.apiclinica.domain.repository.MedicoRepository;
 
 @Service
 public class MedicoService {
@@ -27,5 +26,11 @@ public class MedicoService {
         Page<Medico> medicos = medicoRepository.findAll(page);
         return medicos;
     }
-    
+
+    public Medico pegaMedico(Long id) {
+
+        Medico medico = medicoRepository.findById(id).orElseThrow();
+        return medico;
+    }
+
 }
