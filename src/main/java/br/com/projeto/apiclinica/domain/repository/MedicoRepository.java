08ec243bfd27ce272a57672bde72838e31e192f3
Italo Oliveira <italooliveira-xd@hotmail.com>
2023-06-25 21:46:17 -0300

    Criando o service e o repository para medico.

---
+++ src/main/java/br/com/projeto/apiclinica/domain/repository/MedicoRepository.java
@@ -0,0 +1,11 @@
+package br.com.projeto.apiclinica.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.projeto.apiclinica.domain.models.Medico;
+
+@Repository
+public interface MedicoRepository extends JpaRepository<Medico, Long>{
+    
+}
